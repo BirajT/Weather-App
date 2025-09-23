ba6174cec8ff53f3bb09091b027b48a4c5809c53
BirajT <birajtamrakar803@gmail.com>
2025-09-23 23:13:29 +0545

    API fetch

---
+++ old.js
@@ -0,0 +1,64 @@
+//  <style>
+//     body{
+//         background-color: rgb(15, 15, 36);
+//     }
+//     .red
+// {   
+//     width: 400px;
+//     height: 500px;
+//     border: 2px solid black ;
+//     border-radius: 25px;
+//     border-color: rgb(0, 0, 0);
+//     margin: 200px;
+//     color: rgb(255, 255, 255);
+//     background-color: rgb(53, 177, 171);
+// }
+// .searchbar{
+//     position:fixed;
+//     top: 215px;
+//     left:300px;
+    
+// }
+// .view{
+//     position: fixed;
+//     top:300px;
+//     left:350px;
+// }
+// .temperature
+// {
+//     font-size: 240%;
+// }
+// .city{
+//     font-size: 160%;
+// }
+// .huminity{
+//     margin-top: 430px;
+//     margin-left: 60px;
+// }
+// .speed{
+//     margin-left: 260px;
+//     margin-bottom: 90px;
+// }
+// </style>
+
+// <body>
+//     <div class="red">
+//     <div class="searchbar">
+//         <input type="text">
+//     </div>
+//     <div class="view">
+//     <div class="temperature">
+//         22°c
+//     </div>
+//     <div class="city">
+//         New York
+//     </div>
+//     </div>
+//     <div class="huminity">
+//         64%
+//     </div>
+//     <div class="speed">
+//         18 km/h
+//     </div>
+//     </div>
+// </body>
